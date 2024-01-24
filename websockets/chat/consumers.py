06310600454b13f87dc063593065d590841ccb1c
n4-t3 <nathanmekuria@gmail.com>
2024-01-24 10:51:46 +0900

    implemented message render on the frontend

--- websockets/chat/consumers.py
+++ websockets/chat/consumers.py
@@ -1,30 +1,38 @@
 import json
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from create_chat.models import CreateChat
+from .models import Chat
 
-from channels.generic.websocket import AsyncWebsocketConsumer
-
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
         self.room_id  = self.scope["url_route"]["kwargs"]["uuid"]
         self.room_group_name = f"chat_{self.room_id}"
 
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        user = text_data_json["user"]
+        chatroom = CreateChat.objects.get(id=self.room_id)
+        if chatroom.chat_member_1 == user or chatroom.chat_member_2 == user:
+            chat_db = Chat(chatroom=chatroom, user=user, text=message)
+            chat_db.save()
 
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat.message", "message": message, "sender": user}
         )
 
-    async def chat_message(self, event):
+    def chat_message(self, event):
         message = event["message"]
-        await self.send(text_data=json.dumps({"message": message}))
+        sender = event["sender"]
+        self.send(text_data=json.dumps({"message": message, "sender": sender}))
--- websockets/static/js/scripts.js
+++ websockets/static/js/scripts.js
@@ -1,12 +1,24 @@
 const roomName = document.getElementById('room-id').textContent
+const route = window.location.href
+const route_kwargs = route.split("/")
+const chatMessageParent = document.querySelector(".chat-messages")
+chatMessageParent.scrollTop = chatMessageParent.scrollHeight;
 
 const chatSocket = new WebSocket(
     `ws://${window.location.host}/ws/chat/${roomName}/`
 )
+
 chatSocket.onmessage = (e) =>{
     const data = JSON.parse(e.data)
-    console.log(data)
+    const currentIsSender = data.sender === route_kwargs[5]
+    const newDiv = document.createElement("div")
+    newDiv.className = currentIsSender ? "message sent" : "message received"
+    newDiv.textContent = data.message
+    const chatMessageParent = document.querySelector(".chat-messages")
+    chatMessageParent.appendChild(newDiv)
+    chatMessageParent.scrollTop = chatMessageParent.scrollHeight;
 }
+
 chatSocket.onclose = (e) =>{
     console.error('Chat socket closed unexpectedly.')
 }
@@ -21,7 +33,8 @@ document.querySelector(".send-button").onclick = (e) => {
     const messageInputDom = document.querySelector(".message-input")
     const message = messageInputDom.value
     chatSocket.send(JSON.stringify({
-            'message': message
+            'message': message,
+            'user': route_kwargs[5]
     }))
     messageInputDom.value = ''
 }
